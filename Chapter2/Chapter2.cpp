acab70a0f658469243357e6857533e9acc5a327b
blewis13 <blewis13@DESKTOP-JCHJTL6>
2016-05-14 20:06:11 -0700

    Add project files.

---
+++ Chapter2/Chapter2.cpp
@@ -0,0 +1,20 @@
+// Chapter2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	////////////////////////////////////////////////////
+	//SECTION 2.1 - Primitive Built-In Types
+	////////////////////////////////////////////////////
+
+
+
+	int pause;
+	std::cin >> pause;
+    return 0;
+}
+
